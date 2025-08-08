b9379fbf057ecbe0458dd3d994c2a61192647416
Yomna Mohammed Bassam <133790789+yomnamuhammedd@users.noreply.github.com>
2025-08-09 02:21:14 +0300

    - Updated variables in .env.example - Update main.py to include llm and embedding

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from Database.vectordb.Qdrant import Qdrant
+from LLMs.Embedding import e5Model
+from LLMs.Gemini import GEMINI
 from Config.config import get_settings
 from Routes.data import data_router
 
@@ -11,6 +13,8 @@ async def lifespan(app: FastAPI):
     print("Starting up...")
     settings = get_settings()
     app.db_client = Qdrant(db_path=settings.Qdrant_db_path,distance_method=settings.Qdrant_distance_method).connect()
+    app.embedding_client = e5Model(model_name=settings.EMBEDDING_MODEL_NAME)
+    app.llm = GEMINI(model_name=settings.GEMINI_MODEL_NAME)
     
     yield
     
